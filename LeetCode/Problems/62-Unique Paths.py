61679beccc09d0c7872021cc22d020cf69b58525
Melek Elloumi <melek.elloumi@insat.ucar.tn>
2022-11-16 16:56:26 +0100

    leetcode

---
+++ LeetCode/Problems/62-Unique Paths.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0]*n]*m
+        dp[0]=[1]*n
+        for i in range(1,m):
+            dp[i][0]=1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
+        
