a0e85a0e4b4967b80e0954f0c90b3e3b22b56b0f
Melek Elloumi <melek.elloumi@insat.ucar.tn>
2022-11-27 16:51:29 +0100

    leetcode

---
+++ LeetCode/Problems/230-Kth Smallest Element in a BST.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
+        curr = root
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
