76cebe763572c4536bf3fa2c61004374821c2ea0
Melek Elloumi <melek.elloumi@insat.ucar.tn>
2022-11-29 23:36:19 +0100

    leetcode

---
+++ LeetCode/Problems/199-Binary Tree Right Side View.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = collections.deque()
+        if root:
+            q.append(root)
+
+        while q:
+            val = []
+
+            for i in range(len(q)):
+                node = q.popleft()
+                val.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(val[-1])
+        return res
