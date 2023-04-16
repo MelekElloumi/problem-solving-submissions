4701b00c1f55f6c691bfec7616d85b62d179429f
MelekElloumi <melek.elloumi@insat.ucar.tn>
2023-04-16 15:48:39 +0200

    codingame optimization

--- Codingame/Optimization/Wordle.py
+++ Codingame/Optimization/Wordle.py
@@ -1,13 +1,18 @@
 import sys
 import math
-
+from collections import defaultdict
+# Made by Tanvir
 
 word_count = int(input())  # Number of words in the word set
 words=[]
+letter_count=defaultdict(int)
 for word in input().split():
     # word: Word in the word set
-    words.append(word)
-
+    for c in word.lower():
+        letter_count[c]+=1
+letters = sorted([(value,key)for key,value in letter_count.items()],reverse=True)
+#l="etaoinsrhdlucmfywgpbvkxqjz"
+#letters=[(0,x)for x in l]
 answer=[""]*6
 a=-1
 # game loop
@@ -15,12 +20,12 @@ while True:
     for i,s in enumerate(input().split()):
         state = int(s)
         if state==3:
-            answer[i]=chr(97+a)
+            answer[i]=letters[a][1]
         
     a+=1
     if a==25:
-        answer = ['z' if x == "" else x for x in answer]
+        answer = [letters[a][1] if x == "" else x for x in answer]
     if "" not in answer:
         print("".join(answer))
     else:
-        print(chr(97+a)*6)
+        print(letters[a][1]*6)
